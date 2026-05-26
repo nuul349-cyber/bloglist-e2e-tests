183f50418e9e858304849ab5b0bda1257805075d
Nuul <nuul@example.com>
2026-05-26 07:53:41 -0600

    Add test 'Login' tests

--- tests/bloglist_app.spec.js
+++ tests/bloglist_app.spec.js
@@ -1,11 +1,39 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users/', {
+      data: {
+        name: 'Admin',
+        username: 'admin',
+        password: 'passw'
+      }
+    })
+
+
     await page.goto('/')
   })
 
   test('Login form is shown', async ({ page }) => {
     await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByLabel('username').fill('admin')
+      await page.getByLabel('password').fill('passw')
+      await page.getByRole('button', { name:'login' }).click()
+
+      await expect(page.getByText('Admin logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByLabel('username').fill('admin')
+      await page.getByLabel('password').fill('wrongpassw')
+      await page.getByRole('button', { name:'login' }).click()
+
+      await expect(page.getByText('wrong credentials')).toBeVisible()
+    })
   })
 })
